9ebfb426e36e196e44963e3091030c8eeae100e4
VARSHA MALVIYA <140935808+VARSHA-MALVIYA@users.noreply.github.com>
2024-03-19 23:18:21 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,29 +1,114 @@
-const form=document.querySelector('#form');
+ let random = parseInt(Math.random()*100+1);
 
-form.addEventListener('submit',function(e) {
-     e.preventDefault();
+ const userInput = document.getElementById('inputfield');
+ const SubmitButton = document.getElementById('btn');
+ const previousGuess = document.querySelector('#span2');
+ const remainingGuess = document.querySelector('#span');
+ const lowOrHigh = document.querySelector('#lowOrHi');
+ const result = document.querySelector('#result');
 
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const results = document.querySelector('#results');
 
-    if(height==='' || height<=0 || isNaN(height))
+ const p =document.createElement('p');
+
+ let preGuess=[];
+ let numGuess=1;
+
+ let playGame =true;
+
+ if(playGame)
+ {
+    SubmitButton.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess=parseInt(userInput.value);
+        console.log(guess);
+        validateguess(guess);
+    });
+ }
+
+ function validateguess(guess)
+ {
+    if(isNaN(guess))
     {
-        results.innerHTML='please enter a valid height';
+        alert('Please enter a number');
     }
-    else if(weight=== '' || weight<=0 || isNaN(weight))
+    else if(guess<1)
     {
-        results.innerHTML='please enter a valid weight';
+        alert('Number is less than 1');
+    }
+    else if(guess>100)
+    {
+        alert('Number is greater than 100');
     }
     else 
     {
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        // results.innerHTML=`${bmi}`;
-        if(bmi<18.6)
-        results.innerHTML=`${bmi} , <br> you are under weight`;
-        else if(bmi>=18.6 && bmi<=24.9)
-        results.innerHTML=`${bmi}, <br> you are normal person`;
+        preGuess.push(guess);
+        if(numGuess === 11)
+        {
+            displayGuess(guess);
+            displayMessage(`Game Over<br>Random number was ${random}`);
+            endGame();
+        }
         else 
-        results.innerHTML=`${bmi},<br> you are over weight`;
+        {
+            displayGuess(guess);
+            checkguess(guess);
+        }
     }
-});
+
+ }
+
+ function checkguess(guess)
+ {
+        if(guess === random)
+        {
+            displayMessage(`You guessed it right!!!!<br>You won!!`);
+            endGame();
+        }
+        else if(guess<random)
+        {
+            displayMessage(`Your guess is Tooo low`);
+        }
+        else
+        {
+            displayMessage(`Your guess is Tooo high`);
+        }
+ }
+
+ function displayGuess(guess)
+ {
+    userInput.value='';
+    previousGuess.innerHTML += `${guess}  `;
+    numGuess++;
+    remainingGuess.innerHTML = `${11-numGuess}`;
+ }
+
+ function displayMessage(message)
+ {
+    lowOrHigh.innerHTML= `<p>${message}<p>`;
+ }
+
+ function endGame()
+ {
+    userInput.value='';
+    userInput.setAttribute('disabled','');
+    p.classList.add('button');
+    p.innerHTML=`<h4 id="newgame">Start New Game</h4>`;
+    result.appendChild(p);
+    playGame=false;
+    newGame();
+ }
+
+ function newGame()
+ {
+    const newGameBtn=document.getElementById('newgame');
+    newGameBtn.addEventListener('click',function(e){
+        random = parseInt(Math.random()*100+1);
+        userInput.removeAttribute('disabled');
+        preGuess=[];
+        numGuess=1;
+        result.removeChild(p);
+        previousGuess.innerHTML='';
+        remainingGuess.innerHTML=`${11-numGuess}`;
+        playGame=true;
+    });
+ }
